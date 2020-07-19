16586cfad58f2de15984ae3616b3f6c241097e64
kdp <kdp7584@gmail.com>
2020-07-19 16:17:34 +0900

    Update FindComponent.cs

--- FindComponent.cs
+++ FindComponent.cs
@@ -5,35 +5,33 @@ using UnityEngine.UI;
 
 public class FindComponent : MonoBehaviour {
 
-  // Canvas Object
-  public Transform canvas;
-  
-  // Text Object
-  public Text text;
-  
-  // Image Object
-  public Image image;
-  
+	// Canvas Object
+	public Transform canvas;
+
+	// Text Object
+	public Text text;
+
+	// Image Object
+	public Image image;
+
 	// Use this for initialization
 	void Start () {
-    // Find Canvas Transform
-    canvas = GameObject.Find("Canvas").transform;
-    
-    // Find Text Component
-    text = canvas.FindChild("Text").GetComponent<Text>();
-    
-    // Find Image Component
-    image = canvas.FindChild("Image").GetComponent<Image>();
-  }
-  
+		// Find Canvas Transform
+		canvas = GameObject.Find("Canvas").transform;
+
+		// Find Text Component
+		text = canvas.FindChild("Text").GetComponent<Text>();
+
+		// Find Image Component
+		image = canvas.FindChild("Image").GetComponent<Image>();
+	}
+
 	// Update is called once per frame
 	void Update () {
-      // When Key Press
-      if (Input.GetButtonDown("Submit") || Input.GetButtonDown("Jump")) {
-        // Do stuff
-      }
-  }
-  
-  
+		// When Key Press
+		if (Input.GetButtonDown("Submit") || Input.GetButtonDown("Jump")) {
+			// Do stuff
+		}
+	}
 
 }
